9b9bafef7d65bd1bb481466d213ede63b3d99fee
Tyler Moody <tyler@tylermoody.org>
2018-08-16 07:23:12 -0700

    turn on and turn off tests passing

--- chapter3/single_instance/test/LedDriverTest.cpp
+++ chapter3/single_instance/test/LedDriverTest.cpp
@@ -3,8 +3,11 @@
 
 TEST_GROUP(LedDriver)
 {
+    uint16_t leds;
+
     void setup()
     {
+        leds = 0xffff;
     }
 
     void teardown()
@@ -14,7 +17,21 @@ TEST_GROUP(LedDriver)
 
 TEST(LedDriver , LedsOffAfterCreate)
 {
-    uint16_t leds = 0xffff;
     LedDriver_Create(&leds);
+    LONGS_EQUAL(0, leds);
+}
+
+TEST(LedDriver, TurnOnLedOne)
+{
+    LedDriver_Create(&leds);
+    LedDriver_TurnOn(1);
+    LONGS_EQUAL(1, leds);
+}
+
+TEST(LedDriver, TurnOffLedOne)
+{
+    LedDriver_Create(&leds);
+    LedDriver_TurnOn(1);
+    LedDriver_TurnOff(1);
     LONGS_EQUAL(0, leds);
 }
