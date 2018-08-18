91b4439ea23a7857c786047331c801b2dbaa3bd7
Tyler Moody <tyler@tylermoody.org>
2018-08-18 12:45:09 -0700

    parameterized tests - running test suite now checks both normal and inverted logic

--- chapter4/inverted_led_driver/test/LedDriverTest.cpp
+++ chapter4/inverted_led_driver/test/LedDriverTest.cpp
@@ -2,7 +2,7 @@
 #include "LedDriver.h"
 #include "RuntimeErrorStub.h"
 
-static uint16_t expected(uint16_t reg){
+static uint16_t logicLevelsToHardwareLevels(uint16_t reg){
     if (LedDriver_GetLevels() == INVERTED){
         return ~reg;
     }
@@ -11,7 +11,7 @@ static uint16_t expected(uint16_t reg){
 
 TEST_GROUP(LedDriver){
     uint16_t leds;
-    logic_t levels = INVERTED;
+    logic_t levels = NORMAL;
 
     void setup()
     {
@@ -23,112 +23,251 @@ TEST_GROUP(LedDriver){
     }
 };
 
-TEST(LedDriver , LedsOffAfterCreate){
-    leds = expected(0xffff);
+TEST_GROUP(LedDriverInverted){
+    uint16_t leds;
+    logic_t levels = INVERTED;
+
+    void setup(){
+        LedDriver_Create(&leds, levels);
+    }
+
+    void teardown(){
+    }
+};
+
+void LedsOffAfterCreate(uint16_t& leds, const logic_t& levels){
+    leds = logicLevelsToHardwareLevels(0xffff);
     LedDriver_Create(&leds, levels);
-    LONGS_EQUAL(expected(0), leds);
+    LONGS_EQUAL(logicLevelsToHardwareLevels(0), leds);
 }
 
-TEST(LedDriver, TurnOnLedOne){
-    LedDriver_TurnOn(1);
-    LONGS_EQUAL(expected(1), leds);
+TEST(LedDriver, T_LedsOffAfterCreate){
+    LedsOffAfterCreate(leds, levels);
 }
 
-TEST(LedDriver, TurnOffLedOne){
-    LONGS_EQUAL(expected(0), leds);
+TEST(LedDriverInverted, T_LedsOffAfterCreate){
+    LedsOffAfterCreate(leds, levels);
+}
+
+void TurnOnLedOne(const uint16_t& leds){
     LedDriver_TurnOn(1);
-    LONGS_EQUAL(expected(1), leds);
+    LONGS_EQUAL(logicLevelsToHardwareLevels(1), leds);
+}
+
+TEST(LedDriver, T_TurnOnLedOne){
+    TurnOnLedOne(leds);
+}
+
+TEST(LedDriverInverted, T_TurnOnLedOne){
+    TurnOnLedOne(leds);
+}
+
+void TurnOffLedOne(const uint16_t& leds){
+    LONGS_EQUAL(logicLevelsToHardwareLevels(0), leds);
+    LedDriver_TurnOn(1);
+    LONGS_EQUAL(logicLevelsToHardwareLevels(1), leds);
     LedDriver_TurnOff(1);
-    LONGS_EQUAL(expected(0), leds);
+    LONGS_EQUAL(logicLevelsToHardwareLevels(0), leds);
 }
 
-TEST(LedDriver, TurnOnMultipleLeds){
+TEST(LedDriver, T_TurnOffLedOne){
+    TurnOffLedOne(leds);
+}
+
+TEST(LedDriverInverted, T_TurnOffLedOne){
+    TurnOffLedOne(leds);
+}
+
+void TurnOnMultipleLeds(const uint16_t& leds){
     LedDriver_TurnOn(9);
     LedDriver_TurnOn(8);
-    LONGS_EQUAL(expected(0x180), leds);
+    LONGS_EQUAL(logicLevelsToHardwareLevels(0x180), leds);
 }
 
-TEST(LedDriver, AllOn){
+TEST(LedDriver, T_TurnOnMultipleLeds){
+    TurnOnMultipleLeds(leds);
+}
+
+TEST(LedDriverInverted, T_TurnOnMultipleLeds){
+    TurnOnMultipleLeds(leds);
+}
+
+void AllOn(uint16_t& leds){
     LedDriver_TurnOnAll();
-    LONGS_EQUAL(expected(0xffff), leds);
+    LONGS_EQUAL(logicLevelsToHardwareLevels(0xffff), leds);
 }
 
-TEST(LedDriver, TurnOffAnyLed){
+TEST(LedDriver, T_AllOn){
+    AllOn(leds);
+}
+
+TEST(LedDriverInverted, T_AllOn){
+    AllOn(leds);
+}
+
+void TurnOffAnyLed(const uint16_t& leds){
     LedDriver_TurnOnAll();
     LedDriver_TurnOn(8);
     LedDriver_TurnOff(8);
-    LONGS_EQUAL(expected(0xff7f), leds);
+    LONGS_EQUAL(logicLevelsToHardwareLevels(0xff7f), leds);
 }
 
-TEST(LedDriver, LedMemoryNotReadable){
-    leds = expected(0xffff);
-    LedDriver_TurnOn(8);
-    LONGS_EQUAL(expected(0x0080), leds);
+TEST(LedDriver, T_TurnOffAnyLed){
+    TurnOffAnyLed(leds);
+}
 
-    leds = expected(0xffff);
+TEST(LedDriverInverted, T_TurnOffAnyLed){
+    TurnOffAnyLed(leds);
+}
+
+void LedMemoryNotReadable(uint16_t& leds){
+    leds = logicLevelsToHardwareLevels(0xffff);
+    LedDriver_TurnOn(8);
+    LONGS_EQUAL(logicLevelsToHardwareLevels(0x0080), leds);
+
+    leds = logicLevelsToHardwareLevels(0xffff);
     LedDriver_TurnOff(8);
-    LONGS_EQUAL(expected(0x0000), leds);
+    LONGS_EQUAL(logicLevelsToHardwareLevels(0x0000), leds);
+}
+
+TEST(LedDriver, T_LedMemoryNotReadable){
+    LedMemoryNotReadable(leds);
+}
+
+TEST(LedDriverInverted, T_LedMemoryNotReadable){
+    LedMemoryNotReadable(leds);
+}
+
+void UpperAndLowerBounds(const uint16_t& leds){
+    LedDriver_TurnOn(1);
+    LedDriver_TurnOn(16);
+    LONGS_EQUAL(logicLevelsToHardwareLevels(0x8001), leds);
 }
 
 TEST(LedDriver, UpperAndLowerBounds){
-    LedDriver_TurnOn(1);
-    LedDriver_TurnOn(16);
-    LONGS_EQUAL(expected(0x8001), leds);
+    UpperAndLowerBounds(leds);
 }
 
-TEST(LedDriver, OutOfBoundsTurnOnDoesNoHarm){
+TEST(LedDriverInverted, UpperAndLowerBounds){
+    UpperAndLowerBounds(leds);
+}
+
+void OutOfBoundsTurnOnDoesNoHarm(const uint16_t& leds){
     LedDriver_TurnOn(-1);
     LedDriver_TurnOn(0);
     LedDriver_TurnOn(17);
     LedDriver_TurnOn(3141);
-    LONGS_EQUAL(expected(0), leds);
+    LONGS_EQUAL(logicLevelsToHardwareLevels(0), leds);
 }
 
-TEST(LedDriver, OutOfBoundTurnOffDoesNoHarm){
+TEST(LedDriver, OutOfBoundsTurnOnDoesNoHarm){
+    OutOfBoundsTurnOnDoesNoHarm(leds);
+}
+
+TEST(LedDriverInverted, OutOfBoundsTurnOnDoesNoHarm){
+    OutOfBoundsTurnOnDoesNoHarm(leds);
+}
+
+void OutOfBoundsTurnOffDoesNoHarm(const uint16_t& leds){
     LedDriver_TurnOnAll();
     LedDriver_TurnOff(-1);
     LedDriver_TurnOff(0);
     LedDriver_TurnOff(17);
     LedDriver_TurnOff(3141);
-    LONGS_EQUAL(expected(0xffff), leds);
+    LONGS_EQUAL(logicLevelsToHardwareLevels(0xffff), leds);
 }
 
-TEST(LedDriver, OutOfBoundsProducesRuntimeError){
+TEST(LedDriver, OutOfBoundTurnOffDoesNoHarm){
+    OutOfBoundsTurnOffDoesNoHarm(leds);
+}
+
+TEST(LedDriverInverted, OutOfBoundTurnOffDoesNoHarm){
+    OutOfBoundsTurnOffDoesNoHarm(leds);
+}
+void OutOfBoundsProducesRuntimeError(){
     LedDriver_TurnOn(-1);
     STRCMP_EQUAL("LED Driver: out-of-bounds LED",
             RuntimeErrorStub_getLastError());
     LONGS_EQUAL(-1, RuntimeErrorStub_getLastParameter());
 }
+
+TEST(LedDriver, OutOfBoundsProducesRuntimeError){
+    OutOfBoundsProducesRuntimeError();
+}
+
+TEST(LedDriverInverted, OutOfBoundsProducesRuntimeError){
+    OutOfBoundsProducesRuntimeError();
+}
     
-TEST(LedDriver, IsOn){
+void IsOn(){
     CHECK_FALSE(LedDriver_IsOn(11));
     LedDriver_TurnOn(11);
     CHECK_TRUE(LedDriver_IsOn(11));
 }
+    
+TEST(LedDriver, IsOn){
+    IsOn();
+}
 
-TEST(LedDriver, OutOfBoundsLedsAreAlwaysOff){
+TEST(LedDriverInverted, IsOn){
+    IsOn();
+}
+
+void OutOfBoundsLedsAreAlwaysOff(){
     CHECK_TRUE(LedDriver_IsOff(0));
     CHECK_TRUE(LedDriver_IsOff(17));
     CHECK_FALSE(LedDriver_IsOn(0));
     CHECK_FALSE(LedDriver_IsOn(17));
 }
 
-TEST(LedDriver, IsOff){
+TEST(LedDriver, OutOfBoundsLedsAreAlwaysOff){
+    OutOfBoundsLedsAreAlwaysOff();
+}
+
+TEST(LedDriverInverted, OutOfBoundsLedsAreAlwaysOff){
+    OutOfBoundsLedsAreAlwaysOff();
+}
+
+void IsOff(){
     int led = 6;
     CHECK_TRUE(LedDriver_IsOff(led));
     LedDriver_TurnOn(led);
     CHECK_FALSE(LedDriver_IsOff(led));
 }
 
-TEST(LedDriver, TurnOffMultipleLeds){
+TEST(LedDriver, IsOff){
+    IsOff();
+}
+
+TEST(LedDriverInverted, IsOff){
+    IsOff();
+}
+
+void TurnOffMultipleLeds(const uint16_t& leds){
     LedDriver_TurnOnAll();
     LedDriver_TurnOff(9);
     LedDriver_TurnOff(8);
-    LONGS_EQUAL(expected(0xfe7f), leds);
+    LONGS_EQUAL(logicLevelsToHardwareLevels(0xfe7f), leds);
+}
+
+TEST(LedDriver, TurnOffMultipleLeds){
+    TurnOffMultipleLeds(leds);
+}
+
+TEST(LedDriverInverted, TurnOffMultipleLeds){
+    TurnOffMultipleLeds(leds);
+}
+
+void AllOff(const uint16_t& leds){
+    LedDriver_TurnOnAll();
+    LedDriver_TurnOffAll();
+    LONGS_EQUAL(logicLevelsToHardwareLevels(0x0), leds);
 }
 
 TEST(LedDriver, AllOff){
-    LedDriver_TurnOnAll();
-    LedDriver_TurnOffAll();
-    LONGS_EQUAL(expected(0x0), leds);
+    AllOff(leds);
+}
+
+TEST(LedDriverInverted, AllOff){
+    AllOff(leds);
 }
